8c3ce0c2de94300e8856d913be2ca236f8884081
Hom Dahal <homdahal997@gmail.com>
2024-05-23 20:26:48 -0400

    added route for get and post comments

--- routes/comments.js
+++ routes/comments.js
@@ -5,9 +5,36 @@ const comments = require("../data/comments");
 const error = require("../utilities/error");
 
 router
-    .route("/")
-    .get((req, res) => {
-        res.json(comments);
-    });
+    .route('/')
+    .get((req, res, next) => {
+        try {
+            if (req.query.userId) {
+                const userComments = comments.filter(c => c.userId == req.query.userId);
+                res.json(userComments);
+            } else if (req.query.postId) {
+                const postComments = comments.filter(c => c.postId == req.query.postId);
+                res.json(postComments);
+            } else {
+                res.json(comments);
+            }
+        } catch (error) {
+            next(error);
+        }
+    })
+    .post((req, res, next) => {
+        try {
+            const newComment = {
+                id: comments.length + 1,
+                userId: req.body.userId,
+                postId: req.body.postId,
+                body: req.body.body
+            };
+            comments.push(newComment);
+            res.json(newComment);
+        } catch (error) {
+            next(error);
+        }
+    });   
+    
 
 module.exports = router;
